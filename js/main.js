0bac184d4130f6678e6b0bf8e3d97289d50b2071
ArslonRadjabov <arslonradjabovblog@gmail.com>
2022-04-04 13:04:15 +0500

    slider is finished

--- js/main.js
+++ js/main.js
@@ -1,37 +1,131 @@
-const elList = document.querySelector('.slide__list');
+let story_container = document.querySelector('.container-story');
+let wrapper = document.querySelector('.wrapper');
 
+let stories = `
+`;
 
-for (let i = 0; i < objects.length; i++) {
-    let li = document.createElement("li");
-    li.className = "slide__item";
-    li.innerHTML = `
-    <span class="slide__lines">----------</span>
-    <div class="slide__header row">
-        <img class="slide__avatar" src="images/avatarka.jpg" alt="" width="30" height="30">
-        <a class="avatar__text" href="#">Culture</a>
-        <p class="avatar__days">69 нед</p>
-        <div class="slide__btns row">
-            <button class="btn__social"><i class='bx bx-pause'></i></button>
-            <button class="btn__social"><i class='bx bx-volume-mute'></i></button>
-            <button class="btn__social"><i class='bx bx-dots-horizontal-rounded'></i></button>
-        </div>
-    </div>
-    <div class="slide__media">
-        <video class="slide__media__vid" src="${objects[0].media[0]}" alt="video">
-    </div>
-    <div class="slide__footer row">
-      
-      <input class="slide__input" type="text" placeholder="answer apple">
-      
-      <svg aria-label="Direct" class="_8-yf5  messageIcon" color="#dbdbdb" fill="#dbdbdb" height="24" role="img" viewBox="0 0 24 24" width="24"><line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon></svg>
-    </div>
-    <button class="right__btn sliderBtn">
-      <i class='bx bxs-chevron-right'></i>
-    </button>
-    <button class="left__btn sliderBtn">
-      <i class='bx bxs-chevron-left'></i>
-    </button>
+story.forEach((item) => {
+    stories += `
+            <div class="story ell${item.id}">
+            <div class="moreimg stimg${item.id}"" onclick="clickstory(${item.id})">
+              <div class="userstoryimg st${item.id}">
+                <img src="${item.userImg}" alt="videos" />
+              </div>
+              <h4>${item.userName}</h4>
+              <p>21h</p>
+            </div>
+            <div class="container_video" onclick="clickstory(${item.id})">
+              <video src="${item.vid[0]}" class="elvid${item.id}"></video>
+            </div>
 
-    `
-    elList.appendChild(li);
+            <div class="under_video under${item.id}">
+            <div class="story-top">
+              <div class="videotime-top-story">
+              <div class="video_count">
+              </div>
+              </div>
+              <div class="story-top-userimg">
+                <div class="userImgtop">
+                <img src="${item.userImg}" alt="" />
+                </div>
+                <div class="story-control">
+                  <span><i class="bx bx-play"></i></span>
+                  <span><i class="bx bxs-volume-mute"></i></span>
+                  <span><i class="bx bx-dots-horizontal-rounded"></i></span>
+                </div>
+              </div>
+            </div>
+            <div class="story-bottom">
+              <form action="">
+                <input type="text" placeholder="type something.." />
+                </form>
+                <span><i class="bx bxl-telegram"></i></span>
+            </div>
+            </div>
+          </div>
+            `;
+});
+
+wrapper.innerHTML = stories;
+let i = 0;
+let playedid = '';
+let currentId = 0;
+
+function clickstory(id) {
+    currentId = id;
+    if (i == 1) {
+        let clickedstory = document.querySelector('.clickedstory');
+        clickedstory.classList.remove('clickedstory');
+        let usermainimage = document.querySelector('.usermainimage');
+        usermainimage.classList.remove('usermainimage');
+        let underclass = document.querySelector('.under');
+        underclass.classList.remove('under');
+        let elvid = document.querySelector('.elvid' + playedid);
+        elvid.pause();
+        i--;
+    }
+    let under = document.querySelector('.under' + id);
+    under.classList.add('under');
+    let elvid = document.querySelector('.elvid' + id);
+    elvid.play();
+    playedid = id;
+    let moreimg = document.querySelector('.stimg' + id);
+    moreimg.classList.add('usermainimage');
+    let clickedel = document.querySelector('.ell' + id);
+    clickedel.classList.add('clickedstory');
+    i++;
+
+    //   move
+    if (id == 1) {
+        wrapper.style.marginLeft = '470px';
+    } else if (id == 2) {
+        wrapper.style.marginLeft = '235px';
+    } else if (id == 3) {
+        wrapper.style.marginLeft = '0px';
+    } else if (id == 5) {
+        wrapper.style.marginLeft = '-235px';
+    } else if (id == 6) {
+        wrapper.style.marginLeft = '-470px';
+    } else if (id == 7) {
+        wrapper.style.marginLeft = '-705px';
+    } else if (id == 8) {
+        wrapper.style.marginLeft = '-940px';
+    } else if (id == 9) {
+        wrapper.style.marginLeft = '-1175px';
+    } else if (id == 9) {
+        wrapper.style.marginLeft = '-1410px';
+    } else {
+        wrapper.style.marginLeft = '-1410px';
+    }
 }
+setInterval(interval, 15000);
+
+function interval() {
+    if (currentId !== 0) {
+        if (currentId > 1) {
+            currentId++;
+        } else if (currentId == 9) {
+            clickstory(id);
+            currentId--;
+        }
+    }
+}
+interval();
+
+// let pageCount = 10;
+// let hozirgisi = 8;
+
+// currentPage = hozirgisi;
+
+// let start_page = currentPage - 2 <= 0 ? 1 : currentPage - 2;
+
+// if (currentPage + 2 > pageCount) {
+//   start_page -= currentPage + 2 - pageCount;
+// }
+
+// for (let index = start_page; index <= pageCount; index++) {
+//   if (index == start_page + 5) {
+//     break;
+//   }
+//   console.log(index);
+// }
--- js/objects.js
+++ js/objects.js
@@ -1,32 +1,34 @@
-let objects = [{
+const story = [{
         id: 1,
-        media: ["./images/landscape3.mp4", ],
-        name: "Landscapes",
-
-
+        userName: 'Beatudil',
+        vid: ['../images/landscape3.mp4'],
+        userImg: '../images/avatarka3.jpg',
     },
     {
         id: 2,
-        media: ["./images/landscape5"],
-        name: "evaluation yearth siynic status",
+        userName: 'Scarelet',
+        vid: [
+            '../images/landscape4.mp4',
 
+        ],
+        userImg: '../images/avatarka4.jpg',
     },
     {
         id: 3,
-        media: ["./images/landscape5"],
-        name: "HUrry up there",
-
-    },
-    {
-        id: 4,
-        media: ["./images/landscape5"],
-        name: "hull up huggy in the world the same",
+        userName: 'Helena',
+        vid: ['../images/landscape5.mp4', ],
+        userImg: '../images/avatarka4.jpg',
     },
     {
         id: 5,
-        media: ["./images/landscape5,"],
-        name: "worlds high mountin",
-
+        userName: 'Ruby',
+        vid: ['../images/landscape6.mp4', ],
+        userImg: '../images/avatarka3.jpg',
     },
-
-]
+    {
+        id: 6,
+        userName: 'Helloween',
+        vid: ['../images/landscape4.mp4'],
+        userImg: '../images/avatarka2.jpg',
+    }
+];
