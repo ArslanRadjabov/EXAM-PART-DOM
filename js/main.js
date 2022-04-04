c0346d2437205bd8d45fcdaa3c104aa0c5fd2d4f
ArslonRadjabov <arslonradjabovblog@gmail.com>
2022-04-04 13:37:56 +0500

    ss

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let story_container = document.querySelector('.container-story');
-let wrapper = document.querySelector('.wrapper');
+let content = document.querySelector('.content');
 
 let stories = `
 `;
@@ -45,69 +45,3 @@ story.forEach((item) => {
           </div>
             `;
 });
-
-wrapper.innerHTML = stories;
-let i = 0;
-let playedid = '';
-let currentId = 0;
-
-function clickstory(id) {
-    currentId = id;
-    if (i == 1) {
-        let clickedstory = document.querySelector('.clickedstory');
-        clickedstory.classList.remove('clickedstory');
-        let usermainimage = document.querySelector('.usermainimage');
-        usermainimage.classList.remove('usermainimage');
-        let underclass = document.querySelector('.under');
-        underclass.classList.remove('under');
-        let elvid = document.querySelector('.elvid' + playedid);
-        elvid.pause();
-        i--;
-    }
-    let under = document.querySelector('.under' + id);
-    under.classList.add('under');
-    let elvid = document.querySelector('.elvid' + id);
-    elvid.play();
-    playedid = id;
-    let moreimg = document.querySelector('.stimg' + id);
-    moreimg.classList.add('usermainimage');
-    let clickedel = document.querySelector('.ell' + id);
-    clickedel.classList.add('clickedstory');
-    i++;
-
-    //   move
-    if (id == 1) {
-        wrapper.style.marginLeft = '470px';
-    } else if (id == 2) {
-        wrapper.style.marginLeft = '235px';
-    } else if (id == 3) {
-        wrapper.style.marginLeft = '0px';
-    } else if (id == 5) {
-        wrapper.style.marginLeft = '-235px';
-    } else if (id == 6) {
-        wrapper.style.marginLeft = '-470px';
-    } else if (id == 7) {
-        wrapper.style.marginLeft = '-705px';
-    } else if (id == 8) {
-        wrapper.style.marginLeft = '-940px';
-    } else if (id == 9) {
-        wrapper.style.marginLeft = '-1175px';
-    } else if (id == 9) {
-        wrapper.style.marginLeft = '-1410px';
-    } else {
-        wrapper.style.marginLeft = '-1410px';
-    }
-}
-setInterval(interval, 15000);
-
-function interval() {
-    if (currentId !== 0) {
-        if (currentId > 1) {
-            currentId++;
-        } else if (currentId == 9) {
-            clickstory(id);
-            currentId--;
-        }
-    }
-}
-interval();
